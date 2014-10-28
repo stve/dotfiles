8ad7858ccea4d0300b2a3decefb2426a138538ec
Steve Agalloco <steve.agalloco@gmail.com>
2014-10-27 20:23:35 -0400

    added bundle install alias

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -2,3 +2,4 @@ alias rc='script/console'
 alias rg='script/generate'
 alias rd='script/destroy'
 alias cov='/usr/bin/open -a "/Applications/Google Chrome.app" coverage/index.html'
+alias bi="bundle install"
