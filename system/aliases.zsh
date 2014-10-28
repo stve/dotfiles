1f394e3db940658825dbc9791ac99e7d1cfff53c
Steve Agalloco <steve.agalloco@gmail.com>
2014-10-27 20:25:14 -0400

    add launchd aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -8,3 +8,16 @@ then
   alias ll="gls -l --color"
   alias la='gls -A --color'
 fi
+
+alias mstart='launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.mongodb.plist'
+alias mstop='launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.mongodb.plist'
+alias mystart='launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist'
+alias mystop='launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist'
+alias memstart='launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.memcached.plist'
+alias memstop='launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.memcached.plist'
+alias redstart='launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.redis.plist'
+alias redstop='launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.redis.plist'
+alias pgstart='launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist'
+alias pgstop='launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist'
+alias elstart='launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.elasticsearch.plist'
+alias elstop='launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.elasticsearch.plist'
