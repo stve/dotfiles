13ef138126b2c78491694806c8782555c6a753c1
stve <steve.agalloco@gmail.com>
2016-09-19 09:41:53 -0400

    add rs alias for rails server

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,5 +1,6 @@
 alias rc='rails console'
 alias rg='rails generate'
 alias rd='rails destroy'
+alias rs='rails server'
 alias cov='/usr/bin/open -a "/Applications/Google Chrome.app" coverage/index.html'
 alias bi="bundle install"
