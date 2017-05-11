4026eac4cb2526013c8ab9ff1abcb0baf043484d
stve <steve.agalloco@gmail.com>
2017-05-11 16:12:49 -0400

    fix AWS credentials config

--- aws/env.zsh
+++ aws/env.zsh
@@ -1,4 +1,6 @@
 export EC2_HOME="$(brew --prefix ec2-api-tools)/libexec"
 export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.ec2/pk-*.pem | /usr/bin/head -1)"
 export EC2_CERT="$(/bin/ls "$HOME"/.ec2/cert-*.pem | /usr/bin/head -1)"
-export AWS_CREDENTIAL_FILE=$HOME/.ec2/aws-credentials-master
+# export AWS_CREDENTIAL_FILE=$HOME/.ec2/aws-credentials-master
+export AWS_CREDENTIAL_FILE=$HOME/.aws/credentials
+
