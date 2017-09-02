564824f159da83c56e9f3d9e90a0ae8867f1c8d5
Steve Agalloco <steve.agalloco@gmail.com>
2017-09-02 09:44:46 -0400

    add electron-forge autocompletion

---
+++ javascript/electron.zsh
@@ -0,0 +1,3 @@
+# tabtab source for electron-forge package
+# uninstall by removing these lines or running `tabtab uninstall electron-forge`
+[[ -f /usr/local/lib/node_modules/electron-forge/node_modules/tabtab/.completions/electron-forge.zsh ]] && . /usr/local/lib/node_modules/electron-forge/node_modules/tabtab/.completions/electron-forge.zsh
