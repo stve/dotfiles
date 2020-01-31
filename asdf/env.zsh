4754ba73dbf003572f2f4dbac49e7f5372346e6f
Steve Agalloco <steve.agalloco@gmail.com>
2020-01-31 11:44:22 -0500

    Use fixed brew path for asdf before autodetect

--- asdf/env.zsh
+++ asdf/env.zsh
@@ -1,6 +1,10 @@
 autoload -Uz compinit && compinit
 
-export ASDF_DIR=$(brew --prefix asdf)
+if test -d "/usr/local/opt/asdf"; then
+    export ASDF_DIR=/usr/local/opt/asdf
+else
+    export ASDF_DIR=$(brew --prefix asdf)
+fi
 
 . "$ASDF_DIR/asdf.sh"
 . "$ASDF_DIR/etc/bash_completion.d/asdf.bash"
