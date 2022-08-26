5fb7e48d614304914bf015617a21f4b5b6567c28
Steve Agalloco <steve.agalloco@gmail.com>
2022-08-26 15:34:22 -0400

    Remove electron config

--- javascript/electron.zsh
+++
@@ -1,3 +0,0 @@
-# tabtab source for electron-forge package
-# uninstall by removing these lines or running `tabtab uninstall electron-forge`
-[[ -f /usr/local/lib/node_modules/electron-forge/node_modules/tabtab/.completions/electron-forge.zsh ]] && . /usr/local/lib/node_modules/electron-forge/node_modules/tabtab/.completions/electron-forge.zsh
