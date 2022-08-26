0efc584fcac5be830455952b34bddb85456d5283
Steve Agalloco <steve.agalloco@gmail.com>
2022-08-26 15:32:29 -0400

    Cleanup unused system configs

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -11,16 +11,3 @@ fi
 
 alias rm='rm -i'
 alias cl='clear'
-
-alias mstart='launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.mongodb.plist'
-alias mstop='launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.mongodb.plist'
-alias mystart='launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist'
-alias mystop='launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist'
-alias memstart='launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.memcached.plist'
-alias memstop='launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.memcached.plist'
-alias redstart='launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.redis.plist'
-alias redstop='launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.redis.plist'
-alias pgstart='launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist'
-alias pgstop='launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist'
-alias elstart='launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.elasticsearch.plist'
-alias elstop='launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.elasticsearch.plist'
