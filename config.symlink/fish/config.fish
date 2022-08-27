72c948a4e3010cf2387f712ba7f76e12bb9794c9
Steve Agalloco <steve.agalloco@gmail.com>
2022-08-27 14:16:48 -0400

    Move gopath definition

--- config.symlink/fish/config.fish
+++ config.symlink/fish/config.fish
@@ -1,5 +1,7 @@
 if status is-interactive
     set -gx EDITOR code
+    set -gx PROJECTS ~/Projects
+    set -gx GOPATH "$PROJECTS/go"
 
     # Commands to run in interactive sessions can go here
     direnv hook fish | source
