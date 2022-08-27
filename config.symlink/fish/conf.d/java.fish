d5507b23e91e404262fa322192cdfdfeaedb6d31
Steve Agalloco <steve.agalloco@gmail.com>
2022-08-27 14:18:24 -0400

    Update java home path

---
+++ config.symlink/fish/conf.d/java.fish
@@ -0,0 +1,3 @@
+if test -x /usr/libexec/java_home
+  set -gx JAVA_HOME (/usr/libexec/java_home)
+end
--- java/env.zsh
+++
@@ -1,1 +0,0 @@
-test -x /usr/libexec/java_home && export JAVA_HOME="$(/usr/libexec/java_home)" 2> /dev/null
