e5a039cb3e895fac9375098a73ef65b696989cfb
Steve Agalloco <steve.agalloco@gmail.com>
2024-03-23 21:46:05 -0400

    Move env vars to conf.d
    conf.d is loaded before config.fish. As a result, the env vars need to be
    placed in conf.d otherwise they are not available to other scripts. This
    became problematic for path.fish because it was referencing env vars that
    were not defined.

--- config.symlink/fish/config.fish
+++ config.symlink/fish/config.fish
@@ -1,11 +1,6 @@
 set -U fish_greeting "🐟"
 
 if status is-interactive
-    set -gx EDITOR code
-    set -gx PROJECTS ~/Projects
-    set -gx GOPATH "$PROJECTS/go"
-    set -gx GPG_TTY (tty)
-
     # Commands to run in interactive sessions can go here
     direnv hook fish | source
     starship init fish | source
