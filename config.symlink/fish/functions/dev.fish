54f1cc9cb45d951894bfb4b83df681aea63f2562
Steve Agalloco <steve.agalloco@gmail.com>
2026-07-05 16:07:49 -0400

    Discover development procfiles

--- config.symlink/fish/functions/dev.fish
+++ config.symlink/fish/functions/dev.fish
@@ -1,10 +1,18 @@
 # alias dev='foreman start -f Procfile.development'
 function dev
+    set -f procfile "Procfile"
+
+    if test -f "Procfile.development"
+        set -f procfile "Procfile.development"
+    else if -f "Procfile.development"
+        set -f procfile "Procfile.dev"
+    end
+
     if type -q overmind
-        overmind start
+        overmind start -f $procfile
     else if type -q forego
-        forego start
+        forego start -f $procfile
     else if type -q foreman
-        foreman start
+        foreman start -f $procfile
     end
 end
