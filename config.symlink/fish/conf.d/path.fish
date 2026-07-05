27b05a0431a9d5bfa309f618f833ea84190f5eff
Steve Agalloco <steve.agalloco@gmail.com>
2026-07-05 14:40:53 -0400

    Add dynamic bundle path for bundled gems

--- config.symlink/fish/conf.d/path.fish
+++ config.symlink/fish/conf.d/path.fish
@@ -21,3 +21,20 @@ end
 if test -d /Applications/Chia.app/Contents/Resources/app.asar.unpacked/daemon
   fish_add_path /Applications/Chia.app/Contents/Resources/app.asar.unpacked/daemon
 end
+
+function dynamic_path --on-variable PWD
+    set -l target_dir "$PWD/.bundle/binstubs"
+
+    # Check if the directory exists
+    if test -d $target_dir
+        # Check if it is already in PATH to avoid duplicates
+        if not contains $target_dir $PATH
+            set -gx PATH $target_dir $PATH
+        end
+    else
+        # Optional: Remove it from PATH if you leave the directory
+        if contains $target_dir $PATH
+            set -e PATH[(contains -i $target_dir $PATH)]
+        end
+    end
+end
